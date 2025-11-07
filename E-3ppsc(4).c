6fe4f8ec43bbe0c0aa07c20a28670755e0220b2b
dhanesswar <bandidhaneswar08@gmail.com>
2025-11-07 13:45:32 +0530

    Add files via upload

---
+++ E-3ppsc(4).c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+int main() 
+{ 
+
+float side, area; 
+printf("Enter the side of the square: "); 
+scanf("%f", &side); 
+area = side * side;   // Formula: side² 
+printf("Area of Square = %.2f\n", area);
+return 0;
+}
