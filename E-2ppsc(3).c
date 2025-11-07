1c09f17b6bf83be972a077bdb91cfb3a85e73d11
dhanesswar <bandidhaneswar08@gmail.com>
2025-11-07 13:27:54 +0530

    Add files via upload

---
+++ E-2ppsc(3).c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+int main() 
+{ 
+int a, b; 
+printf("Enter two numbers: "); 
+scanf("%d%d", &a, &b); 
+printf("\n--- Bitwise Operations ---\n"); 
+printf("a & b = %d\n", a & b);   // AND 
+printf("a | b = %d\n", a | b);   // OR 
+printf("a ^ b = %d\n", a ^ b);   // XOR 
+printf("~a = %d\n", ~a);         
+// NOT 
+printf("a << 1 = %d\n", a << 1); // Left shift 
+printf("a >> 1 = %d\n", a >> 1); // Right shift 
+return 0; 
+} 
+
