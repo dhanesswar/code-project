a100ebbea69db7e8dec971a22ea10800f1b94066
dhanesswar <bandidhaneswar08@gmail.com>
2025-11-23 11:34:35 +0530

    Add files via upload

---
+++ Area of Triangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int length=4,breadth=6,area;
+	area=length*breadth;
+	printf("Length is :%d\n",length);
+	printf("Breadth is:%d\n",breadth);
+	printf("Area of Triangle:%d",area);
+	return 0;
+	
+	
+}
