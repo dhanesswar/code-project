b86b0201c57f931c645910ab6d89bed375dc0a42
dhanesswar <bandidhaneswar08@gmail.com>
2025-11-24 18:58:15 +0530

    Add files via upload

---
+++ check eligibility to vote.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("Enter your age:");
+	scanf("%d",&age);
+	if (age>120){
+		printf("Invalid age");
+	}
+	else if (age<0){
+		printf("Invalid age");
+	}
+	else if (age>=18){
+		printf("You are eligible to vote");
+	}
+	else {
+		printf("You are not eligible to vote\ntry  again next year");
+	}
+	return 0;
+}
