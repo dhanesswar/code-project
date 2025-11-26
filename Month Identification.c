476b8e0f0f9508b862c5ca7d67117a6b154ccf4d
dhanesswar <bandidhaneswar08@gmail.com>
2025-11-26 10:51:22 +0530

    Add files via upload

---
+++ Month Identification.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    printf("Enter the number between 1 and 12:");
+    scanf("%d",&N);
+    switch (N){
+        case 1:
+        printf("January");
+        break;
+        case 2:
+        printf("February");
+        break;
+        case 3:
+        printf("March");
+        break;
+        case 4:
+        printf("April");
+        break;
+    case 5:
+        printf("May");
+        break;
+        case 6:
+        
+        printf("June");
+        break;
+        case 7:
+        printf("July");
+        break;
+        case 8:
+        printf("August");
+        break;
+        case 9:
+        printf("Septmber");
+        break;
+        case 10:
+        printf("October");
+        break;
+        case 11:
+        printf("November");
+        break;
+        case 12:
+        printf("Decmber");
+        break;
+        default:
+        printf("Invalid age");
+        break;
+        
+        
+    }
+   
+    return 0;
+}
