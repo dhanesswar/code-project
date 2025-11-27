5469ce747f794bff3d011588be46f4d425a278e6
dhanesswar <bandidhaneswar08@gmail.com>
2025-11-27 21:07:53 +0530

    Update Area of Triangle.c

--- Area of Triangle.c
+++ Area of Triangle.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int length=4,breadth=6,area;
+	int length,breadth,area;
+	printf("Length:");
+	scanf("%d",&length);
+	printf("Breadth:");
+	scanf("%d",&breadth);
 	area=length*breadth;
-	printf("Length is :%d\n",length);
-	printf("Breadth is:%d\n",breadth);
+	
+	
 	printf("Area of Triangle:%d",area);
 	return 0;
 	
 	
 }
+
