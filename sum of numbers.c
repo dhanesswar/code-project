91690ece86c9b65dd6690555da0492c175443d72
dhanesswar <bandidhaneswar08@gmail.com>
2026-07-27 18:59:07 +0530

    Update sum of numbers.c

--- sum of numbers.c
+++ sum of numbers.c
@@ -2,8 +2,8 @@
 int main(){
 	int x,y=0;
 		printf("Enter the number:");
-	scanf("%d",&x);
-	for(x=1;x<=100;x++){
+	scanf("%d",&n);
+	for(x=1;x<=n;x++){
 		y=y+x;
 	}
 	printf("The sum of numbers is:%d",y);
